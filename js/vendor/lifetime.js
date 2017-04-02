2d6f9b9a9ec9e5798cb95da5431ba4141ca5cbef
SeedAction <anselin.lohner@gmail.com>
2017-04-02 10:06:14 +0200

    Initalize project

---
+++ js/vendor/lifetime.js
@@ -0,0 +1,20 @@
+// Lifetime is a js plugin which allows given user to estimate its life
+// expectency according to various criteria.
+
+// Instanciating current user TODO: Make this dynamic
+var currentUser = {
+  firstName : "John",
+  lastName : "Doe",
+  age : 25
+};
+
+// Returns estimated life expectency in seconds
+// score: int ; the final test's result
+function estimate(score) {
+  var lifeExpectency = score; //TODO: Improve this equation
+  return lifeExpectency;
+};
+
+function addToScore(score) {
+
+};
